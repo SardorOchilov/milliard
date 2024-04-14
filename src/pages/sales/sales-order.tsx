b49d3ff4917d60c47d81839c5e4139a58d8f83ea
SardorOchilov <ochilovsardor45@gmail.com>
2024-04-14 14:21:46 +0500

    fix

--- src/pages/sales/sales-order.tsx
+++ src/pages/sales/sales-order.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Button, Input, Spin, Table, TableColumnsType } from "antd";
+import { Button, Input, Select, Spin, Table, TableColumnsType } from "antd";
 import "./sales-order.scss";
 import { Api as ApiSalesOrder } from "@modules/sales/sales";
 import { useQuery } from "react-query";
@@ -10,10 +10,43 @@ const SalesOrder = () => {
   const { t } = useTranslation();
 
   const [salesData, setSalesData] = useState<any[]>([]);
-  const [itemCode, setItemCode] = useState<string>("");
-  const [managerName, setManagerName] = useState<string>("");
+  const [itemCode, setItemCode] = useState<string[]>([]);
+  const [managerName, setManagerName] = useState<string[]>([]);
+  const [itemCodeOptions, setItemCodeOptions] = useState<
+    { label: string; value: string }[]
+  >([{ label: t("all"), value: "all" }]);
+  const [managerOptions, setManagerOptions] = useState<
+    { label: string; value: string }[]
+  >([{ label: t("all"), value: "all" }]);
 
   const sales = useQuery("sales", () => ApiSalesOrder.Get(), {
+    onSuccess: (data) => {
+      const option1: Set<string> = new Set();
+      const option2: Set<string> = new Set();
+
+      data.forEach((item) => {
+        option1.add(item.itemDescription);
+      });
+      const option1Array: { label: string; value: string }[] = Array.from(
+        option1,
+      ).map((item) => ({ label: item, value: item }));
+
+      data.forEach((item) => {
+        option2.add(item.salesEmployeeName);
+      });
+      const option2Array: { label: string; value: string }[] = Array.from(
+        option2,
+      ).map((item) => ({ label: item, value: item }));
+
+      setItemCodeOptions((prevState) => {
+        return [...prevState, ...option1Array];
+      });
+      setManagerOptions((prevState) => {
+        return [...prevState, ...option2Array];
+      });
+      setItemCode(option1Array.map((item) => item.value));
+      setManagerName(option2Array.map((item) => item.value));
+    },
     refetchOnWindowFocus: false,
   });
 
@@ -65,12 +98,11 @@ const SalesOrder = () => {
   useEffect(() => {
     let data: any;
     if (itemCode.length > 0 || managerName.length > 0) {
-      data = sales.data?.filter((item) => {
-        return (
-          item.itemCode.includes(itemCode) &&
-          item.salesEmployeeName.includes(managerName)
-        );
-      });
+      data = sales.data?.filter(
+        (item) =>
+          itemCode.some((code) => item.itemDescription === code) &&
+          managerName.some((value) => item.salesEmployeeName === value),
+      );
     }
 
     setSalesData(data || []);
@@ -113,19 +145,41 @@ const SalesOrder = () => {
     [t, sales.isLoading],
   );
 
+  const handleChangeItemDescription = (value: any) => {
+    if (value.includes("all")) {
+      setItemCode(itemCodeOptions.map((item) => item.value));
+    } else setItemCode(value);
+  };
+
+  const handleChangeManager = (value: any) => {
+    if (value.includes("all")) {
+      setManagerName(managerOptions.map((item) => item.value));
+    } else setManagerName(value);
+  };
+
   return (
     <div className="sales-order-page">
       <h1 className="font-bold text-xl mb-5">{t("sales-order")}</h1>
 
-      <div className={"flex gap-4 mb-5 md:w-1/2"}>
-        <Input
+      <div className={"flex gap-4 mb-5 md:w-1/2 items-start"}>
+        <Select
+          mode="multiple"
+          value={itemCode}
+          style={{ width: "100%" }}
+          allowClear
           placeholder={"item code"}
-          onChange={(e) => setItemCode(e.target.value)}
-        ></Input>
-        <Input
+          onChange={handleChangeItemDescription}
+          options={itemCodeOptions}
+        ></Select>
+        <Select
+          mode="multiple"
+          allowClear
+          value={managerName}
+          style={{ width: "100%" }}
           placeholder={t("sales employee name")}
-          onChange={(e) => setManagerName(e.target.value)}
-        ></Input>
+          onChange={handleChangeManager}
+          options={managerOptions}
+        ></Select>
         <Button type={"primary"} onClick={() => exportToExcel(salesData)}>
           {t("create excel")}
         </Button>
